4263cf689590d6bcbae3af8ddf8e1c850f97c1c1
zhanglei <simple0812@sina.cn>
2017-11-06 10:12:11 +0800

    数据库同步

--- server/models/index.js
+++ server/models/index.js
@@ -4,15 +4,11 @@ var Node = require('./node');
 var config = require('../config');
 
 var db = require('./db');
-var rootdb = require('./rootdb');
 
 exports.Memo = Memo;
 exports.Blog = Blog;
 exports.Node = Node;
 exports.db = db;
 exports.syncDb = function() {
-  return rootdb.query(`CREATE DATABASE IF NOT EXISTS ${config.DB.database} CHARSET utf8`, { raw: true })
-  .then(() => {
-    return db.sync();
-  });
+  return db.sync();
 };
